190b8284abca63617d42fb25fb81815ee1e43c59
Kaeden <33cryptogod@gmail.com>
2024-02-27 17:13:40 -0700

    j

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -20,10 +20,10 @@ const ChatBox = ({ text, lr}) => {
       <div className="flex w-[100vw]  items-center justify-center">
         <div className={`hidden sm:block w-[70px] h-[70px]  absolute ${lrn}`} style={{ backgroundImage:lr==="left-40"?`url(${Rainn})`:`url(${jess})`,backgroundRepeat:"no-repeat",backgroundPosition:"center",backgroundSize:"contain" }}></div>
         <div className={`sm:hidden w-[40px] h-[40px]  absolute left-4`} style={{ backgroundImage:lr==="left-40"?`url(${Rainn})`:``,backgroundRepeat:"no-repeat",backgroundPosition:"center",backgroundSize:"contain" }}></div>
-        <div className={`hidden sm:block py-4 py-2 max-h-[200px] max-w-[100px] max-w-[] shadow-md w-[fit-content] h-[fit-content] rounded rounded-full  px-2 sm:px-10 flex items-center min-w-[200px] absolute  ${lr} ${color}  h-[50px]`}> 
+        <div className={`hidden sm:block py-4 py-2 max-h-[200px]   shadow-md w-[fit-content] h-[fit-content] rounded rounded-full  px-2 sm:px-10 flex items-center min-w-[200px] absolute  ${lr} ${color}  h-[50px]`}> 
           <p className="text-[14px] lg:text-[24px]">{text}</p>
         </div>
-        <div className={` sm:hidden py-2 max-h-[200px] max-w-[200px]  shadow-md w-[fit-content] h-[fit-content] rounded rounded-full  px-2  flex items-center absolute  ${lr==="left-40"?"left-[65px]":"right-10"} ${color}  h-[50px]`}> 
+        <div className={` sm:hidden py-2 max-h-[200px] max-w-[250px]  shadow-md w-[fit-content] h-[fit-content] rounded rounded-full  px-2  flex items-center absolute  ${lr==="left-40"?"left-[65px]":"right-10"} ${color}  h-[50px]`}> 
           <p className="text-[14px] lg:text-[24px]">{text}</p>
         </div>
        
